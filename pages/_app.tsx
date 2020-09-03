74634084e04c07177737d38dbba8a57e77b9df92
Tuan Tran <trantuan1892@gmail.com>
2020-09-03 10:09:42 +0700

    chore: add typescript, material UI and launch.json

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { AppProps } from "next/app";
+
+import theme from "../src/theme";
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Shorten URL</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
